1635276ab44f4c0f201fbd0f3e0e3493cde00e11
Yasuaki Kudo <yasu@tng.coop>
2023-12-11 00:30:38 +0900

    a

--- dial/app.js
+++ dial/app.js
@@ -13,7 +13,7 @@ class Dial {
 
         // Create 60 notches
         for (let i = 0; i < 60; i++) {
-            let angle = (i * 6) * (Math.PI / 180); // Convert angle to radians
+            let angle = (i * 360 / 60) * (Math.PI / 180); // Convert angle to radians
             let x1 = centerX + radius * Math.cos(angle);
             let y1 = centerY + radius * Math.sin(angle);
             let x2 = centerX + (radius - 10) * Math.cos(angle); // Notch length is 10
@@ -45,15 +45,40 @@ class Dial {
 // Main App Logic
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.dial').forEach(svg => {
-        document.querySelectorAll('.dial').forEach(svg => {
-            const id = svg.id;
-            const radius = parseInt(svg.dataset.radius);
-            const centerX = parseInt(svg.dataset.centerX);
-            const centerY = parseInt(svg.dataset.centerY);
-            
-    
-            const options = { radius, centerX, centerY };
-            new Dial(id, options);
+        const id = svg.id;
+        const radius = parseInt(svg.dataset.radius);
+        const centerX = parseInt(svg.dataset.centerX);
+        const centerY = parseInt(svg.dataset.centerY);
+
+        const options = { radius, centerX, centerY };
+        const dial = new Dial(id, options);
+
+        // Mouse event handlers
+        let isDragging = false;
+        let startAngle = 0;
+
+        svg.addEventListener('mousedown', function(event) {
+            isDragging = true;
+            startAngle = getAngle(event, centerX, centerY);
         });
+
+        svg.addEventListener('mousemove', function(event) {
+            if (isDragging) {
+                const currentAngle = getAngle(event, centerX, centerY);
+                const rotateAngle = currentAngle - startAngle;
+                svg.style.transform = `rotate(${rotateAngle}deg)`;
+            }
+        });
+
+        window.addEventListener('mouseup', function() {
+            isDragging = false;
+        });
+
+        function getAngle(event, centerX, centerY) {
+            const rect = svg.getBoundingClientRect();
+            const mouseX = event.clientX - rect.left - centerX;
+            const mouseY = event.clientY - rect.top - centerY;
+            return Math.atan2(mouseY, mouseX) * (180 / Math.PI);
+        }
     });
 });
